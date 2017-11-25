8653c379a214720527b3b0a662332e733f8ba83e
bibyzan <benrasmonkey@gmail.com>
2017-11-25 01:48:19 -0500

    added adding messages

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/ring-doorbell', function (req, res) {
                     if (err) return console.error(err);
                     console.log(messageInfo)
                 });
-                res.send('doorbell rang');
+                res.send('wasup');
             }
         });
 
@@ -32,31 +32,5 @@ app.get('/',function (req,res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('haiiiiiii');
 
-    login({email: process.env.FACEBOOK_USERNAME, password: process.env.FACEBOOK_PASSWORD, forceLogin: true}, function (err, api) {
-        if(err) return console.error(err);
-
-        api.listen(function (err, message) {
-            console.log(JSON.stringify(message));
-            //message = JSON.parse(message);
-            if (message.threadID === '1759489744121223') {
-                if (message.body.indexOf('add-response') != -1) {
-                    fs.readFile('messages.json', 'utf8', function readFileCallback(err, data){
-                        if (err){
-                            console.log(err);
-                        } else {
-                            var messages = JSON.parse(data); //now it an object
-
-                            if (message.body.indexOf(':') != -1) {
-                                messages.messages.push(message.body.split(":")[1])
-                                var json = JSON.stringify(messages); //convert it back to json
-                                fs.writeFile('messages.json', json, 'utf8'); // write it back
-
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    });
 });
 
