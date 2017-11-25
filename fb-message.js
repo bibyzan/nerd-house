87ad2aa0048faba535748ec5f4fe2906423fb8c8
bibyzan <benrasmonkey@gmail.com>
2017-11-25 01:28:29 -0500

    added adding messages

--- fb-message.js
+++ fb-message.js
@@ -1,24 +1,29 @@
 const login = require("facebook-chat-api");
 var fs = require('fs')
 
-login({email: process.env.FACEBOOK_USERNAME, password: process.env.FACEBOOK_PASSWORD}, function (err, api) {
+login({email: process.env.FACEBOOK_USERNAME, password: process.env.FACEBOOK_PASSWORD, forceLogin: true}, function (err, api) {
     if(err) return console.error(err);
 
-    fs.readFile('messages.json', 'utf8', function readFileCallback(err, data){
-        if (err){
-            console.log(err);
-        } else {
-            var messages = JSON.parse(data); //now it an object
-            //fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back
+    api.listen(function (err, message) {
+        console.log(JSON.stringify(message));
+        //message = JSON.parse(message);
+        if (message.threadID === '1759489744121223') {
+            if (message.body.indexOf('add-response') != -1) {
+                fs.readFile('messages.json', 'utf8', function readFileCallback(err, data){
+                    if (err){
+                        console.log(err);
+                    } else {
+                        var messages = JSON.parse(data); //now it an object
 
-            var randomInt = parseInt(Math.random() * messages.messages.length);
+                        if (message.body.indexOf(':') != -1) {
+                            messages.messages.push(message.body.split(":")[1])
+                            var json = JSON.stringify(messages); //convert it back to json
+                            fs.writeFile('messages.json', json, 'utf8'); // write it back
 
-            api.sendMessage(messages.messages[randomInt],'1759489744121223', function (err, messageInfo) {
-                if (err) return console.error(err);
-                console.log(messageInfo)
-            });
+                        }
+                    }
+                });
+            }
         }
     });
-
-
 });
--- index.js
+++ index.js
@@ -31,30 +31,32 @@ app.get('/',function (req,res) {
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('haiiiiiii');
-});
 
+    login({email: process.env.FACEBOOK_USERNAME, password: process.env.FACEBOOK_PASSWORD, forceLogin: true}, function (err, api) {
+        if(err) return console.error(err);
 
-login({email: process.env.FACEBOOK_USERNAME, password: process.env.FACEBOOK_PASSWORD, forceLogin: true}, function (err, api) {
-    if(err) return console.error(err);
+        api.listen(function (err, message) {
+            console.log(JSON.stringify(message));
+            //message = JSON.parse(message);
+            if (message.threadID === '1759489744121223') {
+                if (message.body.indexOf('add-response') != -1) {
+                    fs.readFile('messages.json', 'utf8', function readFileCallback(err, data){
+                        if (err){
+                            console.log(err);
+                        } else {
+                            var messages = JSON.parse(data); //now it an object
 
-    api.listen(function (err, message) {
-        if (message.threadID === '1759489744121223') {
-            if (message.body.contains('add-response')) {
-                fs.readFile('messages.json', 'utf8', function readFileCallback(err, data){
-                    if (err){
-                        console.log(err);
-                    } else {
-                        var messages = JSON.parse(data); //now it an object
+                            if (message.body.indexOf(':') != -1) {
+                                messages.messages.push(message.body.split(":")[1])
+                                var json = JSON.stringify(messages); //convert it back to json
+                                fs.writeFile('messages.json', json, 'utf8'); // write it back
 
-                        if (message.body.contains(':')) {
-                            messages.messages.push(message.body.split(":")[1])
-                            var json = JSON.stringify(messages); //convert it back to json
-                            fs.writeFile('messages.json', json, 'utf8'); // write it back
-
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
-        }
+        });
     });
 });
+
