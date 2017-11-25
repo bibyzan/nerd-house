089dee54354adeb9ae014c1ed7fc2ada307d0803
bibyzan <benrasmonkey@gmail.com>
2017-11-24 20:46:53 -0500

    added message listens

--- fb-message.js
+++ fb-message.js
@@ -1,12 +1,24 @@
 const login = require("facebook-chat-api");
+var fs = require('fs')
 
-// Create simple echo bot
 login({email: process.env.FACEBOOK_USERNAME, password: process.env.FACEBOOK_PASSWORD}, function (err, api) {
     if(err) return console.error(err);
 
-    api.sendMessage('doorbell ring','1759489744121223', function (err, messageInfo) {
-       if (err) return console.error(err)
-       console.log(messageInfo)
+    fs.readFile('messages.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+            var messages = JSON.parse(data); //now it an object
+            //fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back
+
+            var randomInt = parseInt(Math.random() * messages.messages.length);
+
+            api.sendMessage(messages.messages[randomInt],'1759489744121223', function (err, messageInfo) {
+                if (err) return console.error(err);
+                console.log(messageInfo)
+            });
+        }
     });
+
 
 });
--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const express = require('express');
 const login = require("facebook-chat-api");
 const app = express();
 
-const Nexmo = require('nexmo');
-const nexmo = new Nexmo({
-    apiKey: 'f8387d15',
-    apiSecret: '4bc084afe29778d3'
-});
-
 app.get('/ring-doorbell', function (req, res) {
     login({email: process.env.FACEBOOK_USERNAME, password: process.env.FACEBOOK_PASSWORD}, function (err, api) {
         if(err) return console.error(err);
 
-        api.sendMessage('doorbell ring','1759489744121223', function (err, messageInfo) {
-            if (err) return console.error(err)
-            console.log(messageInfo)
+        fs.readFile('messages.json', 'utf8', function readFileCallback(err, data){
+            if (err){
+                console.log(err);
+            } else {
+                var messages = JSON.parse(data); //now it an object
+                var randomInt = parseInt(Math.random() * messages.messages.length);
+
+                api.sendMessage(messages.messages[randomInt],'1759489744121223', function (err, messageInfo) {
+                    if (err) return console.error(err);
+                    console.log(messageInfo)
+                });
+                res.send('doorbell rang');
+            }
         });
-        res.send('doorbell rang');
+
+
     });
 });
 
@@ -26,4 +31,30 @@ app.get('/',function (req,res) {
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('haiiiiiii');
+});
+
+
+login({email: process.env.FACEBOOK_USERNAME, password: process.env.FACEBOOK_PASSWORD}, function (err, api) {
+    if(err) return console.error(err);
+
+    api.listen(function (err, message) {
+        if (message.threadID === '1759489744121223') {
+            if (message.body.contains('add-response')) {
+                fs.readFile('messages.json', 'utf8', function readFileCallback(err, data){
+                    if (err){
+                        console.log(err);
+                    } else {
+                        var messages = JSON.parse(data); //now it an object
+
+                        if (message.body.contains(':')) {
+                            messages.messages.push(message.body.split(":")[1])
+                            var json = JSON.stringify(messages); //convert it back to json
+                            fs.writeFile('messages.json', json, 'utf8'); // write it back
+
+                        }
+                    }
+                });
+            }
+        }
+    });
 });
